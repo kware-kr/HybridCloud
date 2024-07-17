42b80b5fab08650ef5c44ee5748e9feb38e75666
kware <92065307+kware-kr@users.noreply.github.com>
2024-07-17 17:01:23 +0900

    Delete HybridDeployOptimizer directory

--- HybridDeployOptimizer/src/main/java/com/kware/policy/task/selector/service/dao/H2InitDao.java
+++
@@ -1,7 +0,0 @@
-package com.kware.policy.task.selector.service.dao;
-
-//메모리 DB를 사용한다면 처리하자, 지금은 아님.
-//@Repository //@Repository("이름")으로 처리하고 @Resource(name="이름") 주입한다.
-public class H2InitDao {
-
-}
