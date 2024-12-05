4e0958e82753f01ed8882b873489d3f237d9e03c
kljang <kljang@kware.co.kr>
2024-12-05 14:35:57 +0900

    이미지, 폰트 갱신

--- HybridDeployFront/src/main/java/com/kware/hybrid/service/dao/CommonFeatureDao.java
+++ HybridDeployFront/src/main/java/com/kware/hybrid/service/dao/CommonFeatureDao.java
@@ -11,63 +11,61 @@ import com.kware.hybrid.service.vo.CommonFeatureVO;
 
 @Repository
 public class CommonFeatureDao {
-    private final SqlSession sqlSession;
+	 private final SqlSession sqlSession;
 
-    public CommonFeatureDao(SqlSession sqlSession) {
-        this.sqlSession = sqlSession;
-    }
+	    public CommonFeatureDao(SqlSession sqlSession) {
+	        this.sqlSession = sqlSession;
+	    }
 
-    public int insertCommonFeature(CommonFeatureVO commonFeature) {
-        return sqlSession.insert("commonFeatureMapper.insertCommonFeature", commonFeature);
-    }
+	    public int insertCommonFeature(CommonFeatureVO commonFeature) {
+	        return sqlSession.insert("commonFeatureMapper.insertCommonFeature", commonFeature);
+	    }
 
-    public int updateCommonFeature(CommonFeatureVO commonFeature) {
-        return sqlSession.update("commonFeatureMapper.updateCommonFeature", commonFeature);
-    }
+	    public int updateCommonFeature(CommonFeatureVO commonFeature) {
+	        return sqlSession.update("commonFeatureMapper.updateCommonFeature", commonFeature);
+	    }
 
-    public int deleteCommonFeature(String feaName, String feaSubName) {
-        return sqlSession.update("commonFeatureMapper.deleteCommonFeature", 
-            new CommonFeatureVO() {{
-                setFeaName(feaName);
-                setFeaSubName(feaSubName);
-            }}
-        );
-    }
+	    public int deleteCommonFeature(String feaName, String feaSubName) {
+	        return sqlSession.update("commonFeatureMapper.deleteCommonFeature", 
+	            new CommonFeatureVO() {{
+	                setFeaName(feaName);
+	                setFeaSubName(feaSubName);
+	            }}
+	        );
+	    }
 
-    public CommonFeatureVO selectCommonFeatureByKey(String feaName, String feaSubName) {
-        return sqlSession.selectOne("commonFeatureMapper.selectCommonFeatureByKey", 
-            new CommonFeatureVO() {{
-                setFeaName(feaName);
-                setFeaSubName(feaSubName);
-            }}
-        );
-    }
+	    public CommonFeatureVO selectCommonFeatureByKey(String feaName, String feaSubName) {
+	        return sqlSession.selectOne("commonFeatureMapper.selectCommonFeatureByKey", 
+	            new CommonFeatureVO() {{
+	                setFeaName(feaName);
+	                setFeaSubName(feaSubName);
+	            }}
+	        );
+	    }
 
-    public List<CommonFeatureVO> selectAllCommonFeatures(String feaName) {
-        return sqlSession.selectList("commonFeatureMapper.selectAllCommonFeatures", feaName);
-    }
-    
-    
-    // {{mo_cluster 클러스터 특성관련
-    public List<ClusterNodeFeatureVO> selectAllClusterFeatures() {
-        return sqlSession.selectList("commonFeatureMapper.selectAllClusterFeatures");
-    }
-    
-    public int updateClusterFeature(ClusterNodeFeatureVO vo) {
-        return sqlSession.update("commonFeatureMapper.updateClusterFeature", vo);
-    }
-    // }}mo_cluster 클러스터 특성관련
-    
- // {{mo_cluster_node 클러스터 특성관련
-    public List<ClusterNodeFeatureVO> selectAllClusterNodeFeatures() {
-        return sqlSession.selectList("commonFeatureMapper.selectAllClusterNodeFeatures");
-    }
-    
-    public int updateClusterNodeFeature(ClusterNodeFeatureVO vo) {
-        return sqlSession.update("commonFeatureMapper.updateClusterNodeFeature", vo);
-    }
-    // }}mo_cluster 클러스터 특성관련
-
-    
+	    public List<CommonFeatureVO> selectAllCommonFeatures(String feaName) {
+	        return sqlSession.selectList("commonFeatureMapper.selectAllCommonFeatures", feaName);
+	    }
+	    
+	    
+	    // {{mo_cluster 클러스터 특성관련
+	    public List<ClusterNodeFeatureVO> selectAllClusterFeatures() {
+	        return sqlSession.selectList("commonFeatureMapper.selectAllClusterFeatures");
+	    }
+	    
+	    public int updateClusterFeature(ClusterNodeFeatureVO vo) {
+	        return sqlSession.update("commonFeatureMapper.updateClusterFeature", vo);
+	    }
+	    // }}mo_cluster 클러스터 특성관련
+	    
+	 // {{mo_cluster_node 클러스터 특성관련
+	    public List<ClusterNodeFeatureVO> selectAllClusterNodeFeatures() {
+	        return sqlSession.selectList("commonFeatureMapper.selectAllClusterNodeFeatures");
+	    }
+	    
+	    public int updateClusterNodeFeature(ClusterNodeFeatureVO vo) {
+	        return sqlSession.update("commonFeatureMapper.updateClusterNodeFeature", vo);
+	    }
+	    // }}mo_cluster 클러스터 특성관련
     
 }
