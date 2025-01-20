d3be3d46165426738d61c49e482f10a365dee47c
kljang <kljang@kware.co.kr>
2025-01-20 11:21:36 +0900

    설정 정보 변경

--- HybridDeployFront/src/main/java/com/kware/hybrid/service/CommonFeatureService.java
+++ HybridDeployFront/src/main/java/com/kware/hybrid/service/CommonFeatureService.java
@@ -2,14 +2,40 @@ package com.kware.hybrid.service;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.kware.hybrid.service.dao.CommonFeatureDao;
 import com.kware.hybrid.service.vo.ClusterNodeFeatureVO;
 import com.kware.hybrid.service.vo.CommonFeatureVO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class CommonFeatureService {
+	
+	private RestTemplate restTemplate;
+
+    @Value("${hybrid.proxy.url:null}")
+    private String targetUrl;
+    
+    private String proxySettingChangeUrl = null;
+    
+    @PostConstruct
+    private void init() {
+    	this.restTemplate = new RestTemplate();
+    	if(targetUrl.endsWith("/"))
+    		proxySettingChangeUrl = targetUrl + "interface/common/setting/change";
+    	else
+    		proxySettingChangeUrl = targetUrl + "/interface/common/setting/change";
+    	
+    	//proxySettingChangeUrl = proxySettingChangeUrl.replaceAll("//", "/");
+    }
+    
     private final CommonFeatureDao commonFeatureDao;
 
     public CommonFeatureService(CommonFeatureDao commonFeatureDao) {
@@ -17,15 +43,21 @@ public class CommonFeatureService {
     }
 
     public int insertCommonFeature(CommonFeatureVO commonFeature) {
-        return commonFeatureDao.insertCommonFeature(commonFeature);
+        int rst = commonFeatureDao.insertCommonFeature(commonFeature);
+        sendSettingChange("feature");
+        return rst;
     }
 
     public int updateCommonFeature(CommonFeatureVO commonFeature) {
-        return commonFeatureDao.updateCommonFeature(commonFeature);
+    	int rst = commonFeatureDao.updateCommonFeature(commonFeature);
+    	sendSettingChange("feature");
+    	return rst;
     }
 
     public int deleteCommonFeature(String feaName, String feaSubName) {
-        return commonFeatureDao.deleteCommonFeature(feaName, feaSubName);
+    	int rst = commonFeatureDao.deleteCommonFeature(feaName, feaSubName);
+    	sendSettingChange("feature");
+    	return rst;
     }
 
     public CommonFeatureVO getCommonFeatureByKey(String feaName, String feaSubName) {
@@ -43,7 +75,9 @@ public class CommonFeatureService {
     }
     
     public int updateClusterFeature(ClusterNodeFeatureVO vo) {
-        return commonFeatureDao.updateClusterFeature(vo);
+    	int rst = commonFeatureDao.updateClusterFeature(vo);
+    	sendSettingChange("cluster");
+    	return rst;
     }
     // }}mo_cluster 클러스터 특성관련
     
@@ -53,7 +87,21 @@ public class CommonFeatureService {
     }
     
     public int updateClusterNodeFeature(ClusterNodeFeatureVO vo) {
-        return commonFeatureDao.updateClusterNodeFeature(vo);
+    	int rst = commonFeatureDao.updateClusterNodeFeature(vo);
+    	sendSettingChange("node");
+    	return rst;
     }
     // }}mo_cluster 클러스터 특성관련
+    
+	//설정값이 변경되면 백엔드 서버에 변경사실을 전달한다.
+	private void sendSettingChange(String gubun) {
+		new Thread(() -> {
+			try {
+		        String url = proxySettingChangeUrl+ "?g=" + gubun; 
+		        restTemplate.getForObject(url, Void.class);
+			}catch(Exception e) {
+				log.error("API call to notify the Optimizer server of configuration changes failed. Error:", e);
+			}
+		}).start();
+    }
 }
