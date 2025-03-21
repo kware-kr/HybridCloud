1949730d440ca599b93314874eee4c8c85e824b7
kljang <kljang@kware.co.kr>
2025-03-21 16:07:57 +0900

    웹소켓 추가

---
+++ HybridDeployOptimizer/src/main/java/com/kware/common/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.kware.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+   @Override
+   public void registerStompEndpoints(StompEndpointRegistry registry) {
+      // 클라이언트가 연결할 엔드포인트 (SockJS 지원)
+      registry.addEndpoint("/interface/ws")
+              .setAllowedOrigins("172.30.1.28")
+              .withSockJS();
+   }
+
+   @Override
+   public void configureMessageBroker(MessageBrokerRegistry registry) {
+      // 클라이언트가 구독할 토픽 및 애플리케이션 접두사 설정
+      registry.enableSimpleBroker("/topic");
+      registry.setApplicationDestinationPrefixes("/app");
+   }
+}
